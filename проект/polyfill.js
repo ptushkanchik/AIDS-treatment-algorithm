e9235219253b6e57ac974ea2bcb930d82b7756d4
ptushkanchik <ptushkin83@mail.ru>
2016-02-09 22:43:30 +0200

    The polyfill for "elem.remove()".

---
+++ проект/polyfill.js
@@ -0,0 +1,9 @@
+
+//полифилл эмулирует метод elem.remove() для IE
+if(!Element.prototype.remove){
+	Element.prototype.remove = function(){
+		var x = this;
+		var y = this.parentNode;
+		y.removeChild(x);
+		}	
+}
