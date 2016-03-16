604405bfc01d3a270dbe70b1d03131dedc5a0b62
ptushkanchik <ptushkin83@mail.ru>
2016-03-16 14:45:01 +0200

    DOM-properties of elements have been replaced by jQuery's methods

--- проект/functions for opportunity.js
+++ проект/functions for opportunity.js
@@ -1,20 +1,11 @@
 ﻿//функция очистки стартового поля и вывода меню оппортунистических инфекций
 function appendParagraphOpportunity(){
 	
-	$("#start_window").empty();//если в стартовом окне что-то есть - очистить стартовое окно
-		
-	
-	start_window.style.height = "563px";
-	
+	$("#start_window").empty().css("height","563px");//очистить стартовое окно и вернуть высоту окна к исходной	
 //создание параграфа
-    	
-	var attention = document.createElement("div");
-		attention.id = "paragraphOpportunity";
-		attention.style.cssText = "opacity:0; ";
-		attention.innerHTML = "<span style=\"color:red\">ВНИМАНИЕ!</span>  &nbsp В данном разделе рассмотрено <b><u>только лечение</u></b> наиболее специфичных для ВИЧ-инфицированных пациентов оппортунистических инфекций. Заболевания, характерные для широкого круга пациентов \(сальмонеллез, бактериальные пневмонии и т.д.\), а также опухоли \(саркома Капоши, лимфома\) и туберкулез не рассматриваются. ";
-   start_window.appendChild(attention);
-   $("#paragraphOpportunity").animate({opacity:"1"},1000);
-   appendTabOpportunity();//функция создание таблицы выбора инфекции
+    var attention = document.createElement("div");
+		$(attention).attr("id","paragraphOpportunity").css("opacity","0").append("<span style=\"color:red\">ВНИМАНИЕ!</span>  &nbsp В данном разделе рассмотрено <b><u>только лечение</u></b> наиболее специфичных для ВИЧ-инфицированных пациентов оппортунистических инфекций. Заболевания, характерные для широкого круга пациентов \(сальмонеллез, бактериальные пневмонии и т.д.\), а также опухоли \(саркома Капоши, лимфома\) и туберкулез не рассматриваются.").appendTo($(start_window)).animate({opacity:"1"},1000);
+	appendTabOpportunity();//функция создание таблицы выбора инфекции
 }
 
 //функция создания таблицы выбора инфекции 
@@ -25,22 +16,18 @@ function appendTabOpportunity(){
   }
   //создание таблицы выбора инфекции
   var table = document.createElement("table");
-  table.id = "oppTab";
-
-  table.style.cssText = "opacity:0;";
+  $(table).attr("id","oppTab").css("opacity","0");
   for(var i=0;i<3;i++){
 		var tr = document.createElement("tr");
 		for(var j=0;j<3;j++){
-			var td = document.createElement("td");
-			td.className= "oppTabTD";
-			tr.appendChild(td);
-			}
-		table.appendChild(tr);
+			$("<td>").addClass("oppTabTD").appendTo($(tr));
 		}
+		table.appendChild(tr);
+	}
 	
 	var arrText = ["<b>ПНЕВМОЦИСТНАЯ ПНЕВМОНИЯ</b>","<b>ТОКСОПЛАЗМОЗ</b>","<b>АТИПИЧНЫЕ МИКОБАКТЕРИОЗЫ</b>","<b>ЦИТОМЕГАЛОВИРУСНАЯ ИНФЕКЦИЯ</b>","<b>ГЕРПЕТИЧЕСКАЯ ИНФЕКЦИЯ 1 и 2 типов</b>","<b>КАНДИДОЗ</b>","<b>КРИПТОСПОРИДИОЗ, МИКРОСПОРИДИОЗ, ИЗОСПОРОЗ</b>","<b>КРИПТОКОККОЗ</b>","<b>ГИСТОПЛАЗМОЗ</b>",];
 		
-	var arrTD = table.querySelectorAll("td");
+	var arrTD = table.querySelectorAll(".oppTabTD");
 	for(var i=0;i<arrTD.length;i++){
 		arrTD[i].innerHTML = arrText[i];		
 		arrTD[i].addEventListener("click",openOppWindow);//функция загрузки окна вывода информации о конкретной инфекции
@@ -55,7 +42,7 @@ function appendTabOpportunity(){
 function openOppWindow(event){
 	event = event || window.event;
 //сделать коллекцию ячеек oppTab
-	var arrTD = oppTab.querySelectorAll("td");
+	var arrTD = $(".oppTabTD");
 	var eventTD;
 //выислить номер ячейки на которой произошло событие и назначить его в переменную eventTD
 	for(var i=0;i<arrTD.length;i++){
@@ -65,15 +52,14 @@ function openOppWindow(event){
 	}
 //если есть таблица выбора инфекции - удалить ее	
 	if(document.getElementById("oppTab")){
-		start_window.removeChild(oppTab);
+		$("#oppTab").remove();
 		}
 //создание окна вывода информации о конкретной инфекции
-	var divOpp = document.createElement("div");
-	divOpp.id = "divOpp";
-	divOpp.style.cssText = "width:0px; height:3px; left:388px";
-	start_window.appendChild(divOpp);
+	$("<div>").attr("id", "divOpp").css({width:"0px", height:"3px", left:"388px"}).appendTo(start_window);
 	
-//растягивается в ширину		
+	
+//растягивается в ширину
+
 		var x = setTimeout(function X(){
 				if(parseInt(divOpp.style.width)<parseInt("697px")){
 					divOpp.style.width = parseInt(divOpp.style.width)+69.7+"px";
@@ -103,27 +89,20 @@ function openOppWindow(event){
 
 //функция создания кнопки возврата в меню инфекций
 function create_button(){
-	var input = document.createElement("input");
-	input.id = "buttonBack";
-	input.type = "button";
-	input.value = "Назад";
-	input.style.cssText="opacity:0";
-	input.onclick = function(){
-		start_window.removeChild(divOpp);//удалить окно вывода информации о конкретной инфекции
-		start_window.removeChild(input);
-		appendTabOpportunity();//загрузить таблицу выбора инфекции
-		};
-	start_window.appendChild(input);
 	setTimeout(function(){
-		input.style.opacity = "1";
-	},450);
+			$("<input>").attr({id:"buttonBack", type:"button", value:"Назад"}).click(function(){
+																							start_window.removeChild(divOpp);//удалить окно вывода информации о конкретной инфекции
+																							start_window.removeChild(this);
+																							appendTabOpportunity();//загрузить таблицу выбора инфекции
+																							}).appendTo($(start_window));
+			},450);
 }
 
 
 
 //функция создания текста информации о пневмоцистной пневмонии
 function loadText(arg){
-	var Opportunity = document.body.querySelectorAll(".Opportunity");
+	var Opportunity = $(".Opportunity");
 	divOpp.innerHTML = Opportunity[arg].innerHTML;
 	
 
